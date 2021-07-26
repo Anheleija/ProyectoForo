e6af2ee49b1f876dc6d4b1b32ae6c6edb580c731
Angel Alan <leija.lopezangel@gmail.com>
2021-07-26 02:43:59 -0600

    Primer commit
    Acabando el 3 paso de simple is better than complex

---
+++ Tutorial/boards/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Topic
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(
+    widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Que pasa por tu mente?'}),
+    max_length=4000, 
+    help_text="Cantidad maxima de 4000 caracteres"
+    )
+
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
---
+++ Tutorial/boards/views.py
@@ -0,0 +1,83 @@
+from boards.forms import NewTopicForm
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Board, Post, Topic
+from django.http import Http404
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+""""
+def new_topic(request, pk): Asi no se hacen
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: get the currently logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+
+    return render(request, 'new_topic.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
+    
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
+
+def board_topics(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request):
+    return HttpResponse('Hello, World!')
+"""""
