9b47691f48bd1c384dbddf142bbdfd27465b9f65
Angel Alan <leija.lopezangel@gmail.com>
2021-07-27 01:31:03 -0600

    GCBV Pagination

--- Tutorial/boards/views.py
+++ Tutorial/boards/views.py
@@ -103,6 +103,21 @@ class BoardListView(ListView):
     context_object_name = 'boards'
     template_name = 'home.html'
 
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+        return queryset
+
 """ 
 def new_post(request):
     if request.method == 'POST':
--- Tutorial/proyecto/urls.py
+++ Tutorial/proyecto/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(
             template_name='password_change_done.html'),
             name='password_change_done'),
+
+    path('boards/<int:pk>/', views.TopicListView.as_view(), name='board_topics'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
    ## path('new_post/', views.new_post, name='new_post'),
